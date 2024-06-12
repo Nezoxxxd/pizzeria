ab5fbfca5233f9b05df0c631e930334150ff4c17
Nezoxxxd <bets070614@gmail.com>
2024-06-12 16:24:04 +0300

    Initial commit

---
+++ src/main/java/com/modsen/pizzeria/PizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.modsen.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modsen/pizzeria/PizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modsen.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
