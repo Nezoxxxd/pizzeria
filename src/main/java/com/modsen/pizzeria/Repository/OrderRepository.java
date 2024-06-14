8aacdb5e9b2995538b273d91993a4abcf12b5bce
boshiro123 <b.shiroki@mail.ru>
2024-06-14 12:04:01 +0300

    fix indentations

--- src/main/java/com/modsen/pizzeria/Repository/OrderRepository.java
+++ src/main/java/com/modsen/pizzeria/Repository/OrderRepository.java
@@ -4,8 +4,8 @@ import com.modsen.pizzeria.domain.Order;
 import com.modsen.pizzeria.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser(User user);
